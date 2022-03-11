782790a1371b777e86c6bbdf6e987d095a6b373e
ajilichayma <chayma.ajili@ensi-uma.tn>
2022-03-11 09:30:00 +0000

    fixing the template file for assume_role & policy attachement

--- example/data_sources.tf
+++
@@ -1,6 +0,0 @@
-data "template_file" "policy" {
-  template = file("./example.json")
-vars = {
-    bucket_name = "wrdpress-code"
-  }
-}
--- example/main.tf
+++ example/main.tf
@@ -1,25 +1,12 @@
 module "aws_iam_module" {
   source        = "../aws_iam_module"
-  create_policy = true
+  managed_policy_arn = "arn:aws:iam::aws:policy/AmazonGlacierReadOnlyAccess"
+  create_policy = false
   policy_name   = "my_new_plicy"
   policy_path   = "/"
-  policy = "${data.template_file.policy.rendered}"
   role_name        = "my_new_role"
   role_path        = "/"
   role_description = ""
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com, s3.amazonaws.com"
-        }
-      },
-    ]
-  })
   tags = {
     RoleDescription = "AWS IAM module"
   }
